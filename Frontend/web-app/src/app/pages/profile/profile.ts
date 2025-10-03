7740e7e4726a985717201e5acbc825d446e3f3b8
Ammarhussain50 <hussainammar205@gmail.com>
2025-10-04 01:04:14 +0500

    Profile Image added

--- Frontend/web-app/src/app/pages/profile/profile.ts
+++ Frontend/web-app/src/app/pages/profile/profile.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Auth } from '../../services/auth';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Eye, EyeClosed, LoaderCircle, LucideAngularModule } from 'lucide-angular';
+import { Eye, EyeClosed, LoaderCircle, LucideAngularModule, Pencil } from 'lucide-angular';
 import { Router } from '@angular/router';
 import { HttpService } from '../../services/http';
 import { IProfile, IProfileResponse } from '../../types/IProfile';
@@ -26,6 +26,7 @@ export class Profile implements OnInit {
   readonly LoaderCircle = LoaderCircle;
   readonly eye = Eye;
   readonly eyeClosed = EyeClosed;
+   readonly Pencil = Pencil;
 
   ngOnInit(): void {
     this.initForm();
@@ -134,5 +135,21 @@ export class Profile implements OnInit {
         }
       }
     });
+  }
+
+  onFileSelected(event: Event) {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      const base64 = reader.result as string;
+      console.log(base64);
+      
+      this.profileForm.patchValue({
+        profileImage: base64
+      });
+    };
+    reader.readAsDataURL(file);
   }
 }
